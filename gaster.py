ea8dd24b938a65d9976f6875d5483c869da93dc2
Lilith/3 <laylagruszka123@gmail.com>
2025-09-18 16:59:15 +0100

    Added linux support for gaster.py

--- gaster.py
+++ gaster.py
@@ -1,11 +1,13 @@
 # Importing the keyboard module which will acess the inputs from the keyboard
 import keyboard
-
 import os
+import pygame
 
 def restart():
-    #os.system("powershell echo hello")
-    os.system("shutdown /r /t 0")
+    if os.name == 'nt':
+        os.system("shutdown /r /t 0")
+    else:
+        os.system("shutdown now")
 
 keyboard.add_word_listener('gaster', restart, ['enter'], True)
 keyboard.wait()
