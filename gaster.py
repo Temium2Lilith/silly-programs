21fb77bff1483edaf40e9c63df2ee9b13e1bb6bd
Lilly.owo <laylagruszka123@gmail.com>
2025-09-18 21:16:43 +0100

    Finished gaster.py
    Changes from prior commit:
        - This repository now uses pygame-ce
        - I made some formatting changes and fixed
          a lot of bugs present in the prior commit
        - Basically re-wrote the entire file lol

--- gaster.py
+++ gaster.py
@@ -1,11 +1,49 @@
-# Importing the keyboard module which will acess the inputs from the keyboard
 import keyboard
-
-import os
+from os import name, system
+import pygame
 
 def restart():
-    #os.system("powershell echo hello")
-    os.system("shutdown /r /t 0")
+    pygame.init()
+    pygame.mixer.init()
+
+    clock = pygame.time.Clock()
+    window = pygame.Window(title="she wing it till I ding it",
+                           size=(500, 500),
+                           always_on_top=True
+                    )
+    screen = window.get_surface()
+    
+    window.show()
+    window.focus()
+    window.set_fullscreen(desktop=True)
+
+    pygame.mixer.music.load("Resources/winging-my-dingaling.wav")
+    image = pygame.image.load("Resources/gayster.jpg")
+    pygame.mixer.music.set_volume(1)
+
+    running = True
+    time_passed = 0.0
+
+    pygame.mixer.music.play()
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        ms = clock.get_time()
+        time_passed += ms / 1000
+
+        if time_passed >= 7:
+            running = False
+
+        screen.blit(image, (0, 0))
+        window.flip()
+        clock.tick(60)
+
+    if name == 'nt':
+        system("echo gay")
+    else:
+        system("shutdown now")
 
 keyboard.add_word_listener('gaster', restart, ['enter'], True)
 keyboard.wait()
